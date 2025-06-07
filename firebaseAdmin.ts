4131e40dbe655e2ac083d52b5d0d274508396b5d
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-06-07 13:20:46 +0530

    final

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,12 +1,12 @@
 import admin from 'firebase-admin'
-import { getApps } from 'firebase-admin/app'
-import serviceAccount from './serviceAccountKey.json' // Make sure tsconfig allows JSON imports
 
-if (!getApps().length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
-  });
-}
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64!, 'base64').toString('utf8')
+)
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
 
 const adminDB = admin.firestore();
 export { admin, adminDB };
